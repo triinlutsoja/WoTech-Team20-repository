897778af129cd2a59620e9b32766bd7ad26278a6
Triin Lutsoja <161822295+triinlutsoja@users.noreply.github.com>
2024-07-07 18:35:44 +0300

    Added a python project file
    I added a python project file unrelated to WoTech team work. It's just for practice and fooling around with. Open the python project and play!

---
+++ Learning_to_git_TEST/just_testing_github.py
@@ -0,0 +1,17 @@
+print("Hi! This is Triin, trying to learn git.")
+print("Let's all go crazy on this python file.")
+print("""Let's all go crazy on this python file.
+Feel free to add print-sentences, if-statements, for-loops.
+And of course totally random mistakes are also welcome.
+I'm just hoping to create a chaos and then try to figure out
+how to handle all the conflict that arise from 
+multiple people working on the same file together.
+""")
+
+def random_action(number):
+    result = number - 5 + 9
+    return result
+
+nr = 12
+outcome = random_action(nr)
+print(f"The result of the method is: {outcome}")
