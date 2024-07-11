782be1fc3dc511dc6b1202fc1954cf0e56843b97
Triin Lutsoja <161822295+triinlutsoja@users.noreply.github.com>
2024-07-11 19:39:20 +0300

    Added the API folder
    I hope I added all necessary files for it to work!

---
+++ Datorium-API/src/main/java/com/datorium/Datorium/API/DatoriumApiApplication.java
@@ -0,0 +1,50 @@
+// Front end: http://localhost:8080/
+
+package com.datorium.Datorium.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@CrossOrigin
+public class DatoriumApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("asd");
+		SpringApplication.run(DatoriumApiApplication.class, args);
+	}
+
+	@GetMapping("/ping")
+	public String ping() {
+		return "pong";
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/sum")  // example: http://localhost:8080/sum?number1=2&number2=5
+	public int sum(@RequestParam(value="number1") int number1, @RequestParam(value="number2") int number2) {
+		return number1 + number2;
+	}
+
+	@GetMapping("/multiply")
+	public int multiply(@RequestParam(value="number1") int number1, @RequestParam(value="number2") int number2) {
+		return number1 * number2;
+	}
+
+	@GetMapping("/array")  // example: http://localhost:8080/array?string1=Estonia&string2=Latvia
+	public String[] array(@RequestParam(value="string1") String string1, @RequestParam(value="string2") String string2) {
+		String[] stringArray = {string1, string2};
+		return stringArray;
+
+	}
+
+
+}
