82cfeea530c9167d0039a579ed59c4aac5fc47ab
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-08-10 12:50:39 +0300

    Update read_data (1).py

--- Pokemon_Lesson/read_data (1).py
+++ Pokemon_Lesson/read_data (1).py
@@ -1,11 +1,11 @@
-def get_max_speed(df, column='Speed'):
+def get_max_value(df, column):
     return df[column].max()
 
-def get_min_speed(df, column='Speed'): 
+def get_min_value(df, column):
     return df[column].min()
 
-def get_speed_difference(df, column='Speed'): 
-    max_value = get_max_speed(df, column)  
-    min_value = get_min_speed(df, column) 
+def get_value_difference(df, column):
+    max_value = get_max_value(df, column)
+    min_value = get_min_value(df, column)
     difference = max_value - min_value
     return difference
