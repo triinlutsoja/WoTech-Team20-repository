6c1dbdc9d3a3208b475b95fe97e41d62d52d6bd1
Kinchtiii <165931659+Kinchtiii@users.noreply.github.com>
2024-08-10 12:51:44 +0300

    Update longest_name.py
    fixed syntax

--- Pokemon_Lesson/longest_name.py
+++ Pokemon_Lesson/longest_name.py
@@ -1,4 +1,4 @@
-def check_name(dataframe):
-    length = dataframe['Name'].str.strip(' ').str.len().idxmax()
-    longest_name = dataframe.iloc[length]['Name']
+def check_name(dataframe, column):
+    length_id = dataframe[column].str.strip(' ').str.len().idxmax()
+    longest_name = dataframe.iloc[length_id][column]
     return longest_name
