dfc0a83e1a38504569faa7b97130244437f80ffb
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-08-10 15:05:18 +0300

    Update Final.py

--- Pokemon_Lesson/Final.py
+++ Pokemon_Lesson/Final.py
@@ -1,25 +1,39 @@
-import pandas as pd
-from max_min_difference import get_max_value, get_min_value, get_value_difference
-from percentage import percentage_of_type
-from dataframe_operations import check_len
 from read_data import read_csv
-from filtering import filter_greater_than_or_equal
-from visualization import create_barh_chart
+from pokemon_water_type import pokemon_water_percentage
+from pokemon_speed_value import min_speed
+from speed_value import max_speed, difference
+from dataframe_length import check_len
+from filter_data import filter_greater_than_or_equal
+from longest_name import check_name
+from visualize_data import create_barh_chart
 
-# Step 1: read document
-file_path = "Pokemon.csv"
-df = read_csv(file_path)
+# Define the path to the CSV file
+file_path = 'Pokemon.csv'
 
+# Read the CSV file
+data = read_csv(file_path)
 
-num_pokemons = check_len(df)
+# Calculate the percentage of Water-type Pokémon
+water_percentage = pokemon_water_percentage(data)
 
+# Calculate the minimum speed
+min_speed_value = min_speed(data)
+
+# Calculate the maximum speed
+max_speed_value = max_speed(data)
+
+# Calculate the difference between max and min speed
+speed_difference = difference(data)
+
+# Get the length of the DataFrame
+data_length = check_len(data)
+
+# Filter Pokémon by speed threshold and print the number of filtered Pokémon
 speed_threshold = 80
-filtered_df = filter_greater_than_or_equal(df, 'Speed', speed_threshold)
+filtered_data = filter_greater_than_or_equal(data, 'Speed', speed_threshold)
 
-max_speed = get_max_value(filtered_df, 'Speed')
-min_speed = get_min_value(filtered_df, 'Speed')
-speed_difference = get_value_difference(filtered_df, 'Speed')
+# Find the longest Pokémon name
+longest_pokemon_name = check_name(data, 'Name')
 
-water_percentage = percentage_of_type(df, 'Type 1', 'Water')
-
-create_barh_chart(filtered_df, x_column='Name', y_column='Speed', df_title='Speed of Pokémon')
+# Create a horizontal bar chart of Pokémon speeds
+create_barh_chart(data, x_column='Name', y_column='Speed', df_title='Speed of Pokémon')
