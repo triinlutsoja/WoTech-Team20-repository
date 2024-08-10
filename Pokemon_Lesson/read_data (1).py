a33dcda5e8dfe7eeb479293d2dcd5ca482bea1ab
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-08-10 13:51:56 +0300

    Update read_data (1).py

--- Pokemon_Lesson/read_data (1).py
+++ Pokemon_Lesson/read_data (1).py
@@ -1,8 +1,6 @@
 def get_max_value(df, column):
     return df[column].max()
 
-def get_min_value(df, column):
-    return df[column].min()
 
 def get_value_difference(df, column):
     difference = get_max_value(df, column) - get_min_value(df, column)
