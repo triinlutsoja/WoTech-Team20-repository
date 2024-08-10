e4f50642350d08623733e00ed7cf26397b644a3b
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-08-10 12:43:21 +0300

    Update read_data (1).py

--- Pokemon_Lesson/read_data (1).py
+++ Pokemon_Lesson/read_data (1).py
@@ -2,8 +2,6 @@ def get_max_speed(df, Speed):
     return df[column].max()
     max_speed = get_max_speed(df, 'Speed')
 
-print(f"The maximum Speed value is: {max_speed}")
-
 def get_min_speed(df, Speed):
     return df[Speed].min()
     min_speed = get_min_speed(df, 'Speed')
