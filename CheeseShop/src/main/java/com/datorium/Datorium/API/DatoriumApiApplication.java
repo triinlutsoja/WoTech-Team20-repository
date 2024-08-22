9b068ebff79288dfc44a541a7614ded17fd5b7a4
Triin Lutsoja <161822295+triinlutsoja@users.noreply.github.com>
2024-08-22 20:41:24 +0300

    Added Oskars' API files

---
+++ CheeseShop/src/main/java/com/datorium/Datorium/API/DatoriumApiApplication.java
@@ -0,0 +1,13 @@
+package com.datorium.Datorium.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatoriumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatoriumApiApplication.class, args);
+	}
+
+}
---
+++ CheeseShop/src/test/java/com/datorium/Datorium/API/DatoriumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datorium.Datorium.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatoriumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
