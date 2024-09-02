a2967c2ec32e29e517402ef18aa0fee482c57e9e
Triin Lutsoja <161822295+triinlutsoja@users.noreply.github.com>
2024-09-02 09:47:53 +0300

    Started to create CRUD methods
    Not ready yet, because unsure about what is expected in the return statements for every method.

--- CheeseShop/src/main/java/com/datorium/Datorium/API/Controllers/CheeseShopController.java
+++ CheeseShop/src/main/java/com/datorium/Datorium/API/Controllers/CheeseShopController.java
@@ -1,4 +1,32 @@
 package com.datorium.Datorium.API.Controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 public class CheeseShopController {
+
+    CheeseShopService cheeseShopService;
+
+    public CheeseShopController() {
+        cheeseShopService = new CheeseShopService();
+    }
+
+    // TODO: Don't forget to specify that the URL should be /cheese-shop for all methods?
+
+    // CRUD methods
+
+    @PostMapping("/add")
+    public Cheese addCheese(Cheese cheese){
+        // TODO:
+    }
+
+    // TODO: getCheese and getAllCheeses
+
+    @PutMapping("/update")
+    // TODO:
+
+    @DeleteMapping("/delete")
+    // TODO:
 }
